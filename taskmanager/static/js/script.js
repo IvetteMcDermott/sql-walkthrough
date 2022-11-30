874fd63a7b43dede0f9e14e820bf14dcad983ee9
IvetteMcDermott <ivettedemcdermott@gmail.com>
2022-11-30 21:51:40 +0000

    Add_task code

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //sidenavbar initalization  
     let sidebar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidebar);
+
+    //datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, { 
+      format: "dd mmmm, yyy",
+      i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
   });
